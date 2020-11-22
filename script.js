d44dd9c9e3adacf433a2a7659509558b1d9ce9f3
Rusheel <rusheel1309@gmail.com>
2020-11-22 11:57:16 +0530

    V1.0

---
+++ script.js
@@ -0,0 +1,32 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZXphbGIiLCJhIjoiY2toc3EyM2N3MHY2aTJycGJxbDRjcDVmOSJ9.sQkAap5XDBgtpMF5rR-c1A"
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true
+})
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+  setupMap([72.50953, 23.00944])
+}
+
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 15
+  })
+
+  const nav = new mapboxgl.NavigationControl()
+  map.addControl(nav)
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+  })
+
+  map.addControl(directions, "top-left")
+}
